4a98abc1dabee3c11d501a8e3c9a7692e9a25ff0
Manvendra2509 <manvendra.17cse468@rtu.ac.in>
2023-06-05 13:20:49 +0530

    Refactor the code

--- app.js
+++ app.js
@@ -1,24 +1,19 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname+ "/date.js");
 
 const app = express();
+
 let items = ["Buy Food", "Cook Food", "Eat Food"];
 let workItems = [];
+
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static("public"));
 
 app.get("/", function (req, res) {
-  let today = new Date();
-
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-  };
-
-  let day = today.toLocaleDateString("en-US", options);
-
+  let day = date.getDate();
   res.render("list", { listTitle: day, newListItems: items });
 });
 
